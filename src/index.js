85b32bab330db8297a74d669dff5404f8a184145
Omkar Ghate <otg0231@gmail.com>
2021-08-08 18:33:10 +0530

    feat: customized label names

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import "./styles.css";
 const btn = document.querySelector("#addBtn");
 const inputList = document.querySelector(".input-list");
-
+var labelName = "";
 btn.addEventListener("click", handleClick);
 var count = 1;
 function handleClick(e) {
   if (count <= 6) {
     //creating new div with class: input-item
+    labelName = prompt("Enter the label name");
     const inputDiv = document.createElement("div");
     inputDiv.classList.add("input-item");
     //inner div with class: input-text, to hold the input tag
     const newItem = document.createElement("div");
     newItem.innerHTML = `
-  <label html-for="name">Name</label>
+  <label html-for="name">${labelName}</label>
   <input id="name" type="text" placeholder="Enter your name"> </input>
   `;
 
